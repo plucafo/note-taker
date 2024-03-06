de1c0da082ba6bd441b0a33b652a32e03f839815
plucafo <plucaf87@gmail.com>
2024-03-06 15:06:58 -0700

    added path module and served notes.html to the user at the /notes route

--- Develop/server.js
+++ Develop/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const db = require('./db/db.json');
 
 const PORT = 3001;
@@ -14,7 +15,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/notes', (req, res) => {
-    res.send('Notes database will display here.');
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 })
 
 app.listen(PORT, () => {
